4eabded5d6bb299f53a087dcf2bf2f961fc23892
david.mora <david.89.mora@gmail.com>
2020-02-15 20:18:10 -0600

    patch 1/2 (front)

--- pages/tablero/_id.vue
+++ pages/tablero/_id.vue
@@ -1,23 +1,64 @@
 <template>
   <div>
+    <div>{{$store.state.user.token}}</div>
+
     <div class="container-list container">
-      <div class="child-list mt-5" v-for="list in this.lister" :key="list.id" :id="'parent'+list.id">
+      <div
+        class="child-list mt-5"
+        v-for="list in this.lister"
+        :key="list.id"
+        :id="'parent'+list.id"
+      >
         <p class="element-list">{{list.nombre}}</p>
 
         <div v-for="card in cards" :key="card.id" :id="'child'+card.id+list.id">
-          <div class="element-card" v-if="card.lista == list.id" >
-            <i style="margin:7px" class="fas fa-grip-vertical" :id="'card' + card.id"></i>
-            {{card.nombre}}
-            <a @click="delateCards(card.id,'parent'+list.id, 'child'+card.id+list.id)" class="button-task">Delate</a>
+          <div class="element-card" v-if="card.lista == list.id" :id="'element-card'+card.id+list.id">
+            <div style="display:inline-block;">
+            <p>{{card.nombre}}</p>
+            </div>
           </div>
+          <div class="showlist"> 
+            <a v-if="card.lista==list.id" :id="'editbtn'+card.id+list.id" class="button-edit" @click="togglehidde('editbtn' + card.id+list.id, 'element-card' + card.id+list.id,'idedit' + card.id+list.id,'btn'+card.id+list.id,'edit')"
+            >Edit</a>
+            <a v-if="card.lista==list.id"
+              @click="delateCards(card.id,'parent'+list.id, 'child'+card.id+list.id)"
+              class="button-delate"
+            >Delate</a>
+            </div>
+            <div v-if="card.lista == list.id" style="dip"><input class="hiddelement" :id="'idedit' + card.id+list.id" type="text" placeholder="Editar" value /> 
+            <a @click="togglehidde('editbtn' + card.id+list.id, 'element-card' + card.id+list.id,'idedit' + card.id+list.id,'btn'+card.id+list.id, 'save')" class="hiddelement button-save" :id="'btn'+card.id+list.id">Save</a>  
+            
+             </div>
         </div>
         <div :id="'card'+ list.id"></div>
         <div class="button-card">
-          <input :id="'id' + list.id" type="text" placeholder="Tarea" value=""/>
+          <input :id="'id' + list.id" type="text" placeholder="Tarea" value />
           <a @click="pushCards('id'+list.id, 'card'+list.id, list.id)" class="button-task">Add</a>
         </div>
       </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.js"></script>
+    <script>
+      token = {{"'"+tkn+"'"}}
+      function delatelem(parent, child, id) {
+      parent.removeChild(child);
+      const url = "/api/api/v1/tarjetas/" + id + "/";
+      const yourConfig = {
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      };
+      axios
+        .delete(url, yourConfig)
+        .then(response => {
+          console.log('elemento eliminado ' + id)
+          return response.data.results;
+       })
+        .catch(() => {
+          alert("error");
+        });
+    }
+    </script>
   </div>
 </template>
 
@@ -36,10 +77,21 @@ export default {
       prevp: 1,
       tablero: [1],
       cards: [1],
-      input1: "tarea"
+      input1: "tarea",
+      cardid: '',
+      cardidarr: [],
+      toptop: ''
     };
   },
   computed: {
+    topid: function(){
+     let val = 0
+    this.cards.forEach(element=>{if(element.id > val){val = element.id}})
+    return val   
+    },
+    tkn: function() {
+      return this.$store.state.user.token.toString();
+    },
     table: function() {
       this.tablero = this.elemdeploy.filter(
         element => element.id == this.$route.params.id
@@ -54,27 +106,69 @@ export default {
       return this.lists;
     }
   },
+
   created() {
     this.getCards();
     this.getTableros();
     this.getListas();
     this.removeElementsByClass("list-group-item");
     this.destroy("hidden123");
+    this.calltopid()
+    this.callcardid()
+    this.getlastid()
   },
   updated() {
     this.destroy("hidden123");
   },
   methods: {
-    delateCards(id,parent, child){
-      const url = "/api/api/v1/tarjetas/"+id+'/';
-      let parent1 = document.getElementById(parent)
-      let child1 = document.getElementById(child)
-      console.log(parent1)
-      console.log(child1)
-      //parent1.removeChild(child1)
+    togglehidde(id,id2,id3,id4,save){
+   console.log(id,id2,id3,id4)
+
+
+  console.log()
+
+var element = document.getElementById(id);
+   element.classList.toggle("hiddelement");
+  
+   var element1 = document.getElementById(id2);
+   element1.classList.toggle("hiddelement");
+console.log(element1.firstChild.innerHTML)
+   var element2 = document.getElementById(id3);
+   element2.classList.toggle("showelement");
+
+   var element3 = document.getElementById(id4);
+   element3.classList.toggle("showelement");
+
+console.log(element2)
+  
+  if(save == 'edit')
+  element2.value = element1.firstChild.firstChild.innerHTML
+  else
+  element1.firstChild.firstChild.innerHTML = element2.value
+
+
+
+
+    },
+    calltopid(){
+      return this.topid
+
+    },
+    callcardid(){
+      return this.cardid
+    },
+    
+    printer() {
+      console.log("hello");
+    },
+    delateCards(id, parent, child) {
+      const url = "/api/api/v1/tarjetas/" + id + "/";
+      let parent1 = document.getElementById(parent);
+      let child1 = document.getElementById(child);
+      parent1.removeChild(child1);
       const yourConfig = {
         headers: {
-          Authorization: "Bearer"+this.$store.state.user.token
+          Authorization: "Bearer" + this.$store.state.user.token
         }
       };
       axios
@@ -85,17 +179,15 @@ export default {
         .catch(() => {
           alert("error");
         });
-
     },
     pushCards(a, b, idd) {
+      var idtemp = "temp" + Date.now();
       var c = document.getElementById(a).value;
       var d = document.getElementById(b).innerHTML;
-      var e =
-        '<div class="element-card" v-if="card.lista == list.id"><i style="margin:7px" class="fas fa-grip-vertical">' +
-        `${c}` +
-        "</i></div>";
-      document.getElementById(b).innerHTML += e;
-      this.pushCards1(c,idd);
+      var b1 = '"' + b.toString() + '"';
+      var idtemp1 = '"' + idtemp.toString() + '"';
+      this.pushCards1(c, idd, idtemp, c,b, b1, idtemp1);
+      //this.getlastid(idtemp, c,b, b1, idtemp1);
     },
 
     destroy(className) {
@@ -149,6 +241,34 @@ export default {
       if (idt == idl) return lista;
       else return "hidden123";
     },
+    getlastid(idtemp,c,b,b1,idtemp1){
+      const yourConfig = {
+        headers: {
+          Authorization: "Bearer " + this.$store.state.user.token
+        } 
+      };
+      axios
+        .get("/api/api/v1/tarjetas1/", yourConfig)
+        .then(response => {
+          this.toptop = response.data.results;
+          var e =
+        '<div id="' +
+        idtemp +
+        '" class="element-card" v-if="card.lista == list.id"><i style="margin:7px" class="fas fa-grip-vertical">' +
+        `${c}` +
+        "</i><a onclick='delatelem(document.getElementById(" +
+        b1 +
+        "),document.getElementById(" +
+        idtemp1 +
+        ")," +
+        this.toptop[0].id +
+        ")' class='button-delate'>Delate</a></div>";
+      document.getElementById(b).innerHTML += e;
+        })
+        .catch(() => {
+          //alert("error");
+        });
+    },
     getCards() {
       const url = "/api/api/v1/tarjetas/";
       const yourConfig = {
@@ -156,27 +276,29 @@ export default {
           Authorization: "Bearer " + this.$store.state.user.token
         }
       };
+      
       axios
         .get(url, yourConfig)
         .then(response => {
           this.cards = response.data.results;
+          
         })
         .catch(() => {
           alert("error");
         });
+        
     },
-    pushCards1(nombre1, id) {
-      console.log(nombre1)
+    pushCards1(nombre1, id, idtemp, c,b, b1, idtemp1) {
       const url = "/api/api/v1/tarjetas/";
       const yourConfigh = {
         headers: {
           Authorization: `Bearer ${this.$store.state.user.token}`
-        }}
-        if(nombre1 == ''){
-          nombre1 = 'empty'
         }
-       
-        
+      };
+      if (nombre1 == "") {
+        nombre1 = "empty";
+      }
+
       const yourConfigb = {
         nombre: nombre1,
         lista: id.toString(),
@@ -185,12 +307,14 @@ export default {
         dueño: "1",
         fecha_creacion: "2020-11-11",
         fecha_vencimiento: "2021-11-11",
-        posicion: "666"}
-        
+        posicion: "666"
+      };
+
       axios
         .post(url, yourConfigb, yourConfigh)
         .then(response => {
-          return response;
+           response
+           this.getlastid(idtemp, c,b, b1, idtemp1);
         })
         .catch(() => {
           alert("error");
@@ -268,13 +392,52 @@ body
     text-align: center
 
 .button-card
-    display:flex
-    align-items:center
+    display: flex
+    align-items: center
     margin: 10px
 
 .button-task
     background: green
     color: black
     padding: 5px
+    cursor: pointer
+    
+
+.button-delate
+    background: red
+    color: black
+    padding: 5px
+    margin: 2px
+    cursor: pointer
+
+.button-edit
+    background: blue
+    color: black
+    padding: 5px
+    margin: 2px
+    cursor: pointer
+
+.button-save
+    background: cyan
+    color: black
+    padding: 5px
+    margin: 2px
+    cursor: pointer
+    display: inline
+
+.showlist
+  display: flex
+  flex-direction: row
+  align-items: center
+  justify-content: space-around
+
+.hiddelement
+  display: none
+
+.showelement
+  display: block
+
+
 
 </style>
+
